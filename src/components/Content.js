baf294ff956782da799a2ef05ad6f24ce84a0504
theblackmonk <86014667+theblackmonk@users.noreply.github.com>
2021-11-12 08:08:29 -0500

    add Buy sell order functionality

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,6 +7,7 @@ import OrderBook from './OrderBook'
 import MyTransactions from './MyTransactions'
 import PriceChart from './PriceChart'
 import Balance from './Balance'
+import NewOrder from './NewOrder'
 
 class Content extends Component {
         componentDidMount() {           //component lifecycle change componentWillMount = componentDidmount
@@ -24,24 +25,16 @@ class Content extends Component {
           return (
             <div className="content">
               <div className="vertical-split">
-              <Balance />
-                <div className="card bg-dark text-white">
-                  <div className="card-header">
-                    Card Title
-                  </div>
-                  <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="/#" className="card-link">Card link</a>
-                  </div>
-                </div>
+                <Balance />
+                <NewOrder />
               </div>
-              <OrderBook />
-              <div className="vertical-split">
+            <OrderBook />
+            <div className="vertical-split">
                 <PriceChart />
                 <MyTransactions />
-              </div>
-              <Trades />
             </div>
+        <Trades />
+      </div>
           )
         }
       }
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -20,7 +20,10 @@ import {
     exchangeEtherBalanceLoaded,
     exchangeTokenBalanceLoaded,
     balancesLoaded,
-    balancesLoading
+    balancesLoading,
+    buyOrderMaking,
+    sellOrderMaking,
+    orderMade
  } from './actions'
 
 // dispatch comes from redux. It means we want to trigger an action and dispatch it with redux
@@ -115,6 +118,10 @@ export const subscribeToEvents = async (exchange, dispatch) => {
   exchange.events.Withdraw({}, (error, event) => {
     dispatch(balancesLoaded())
   })
+
+  exchange.events.Order({}, (error, event) => {
+    dispatch(orderMade(event.returnValues))
+  })
 }
 
 export const cancelOrder = (dispatch, exchange, order, account) => {
@@ -208,6 +215,38 @@ export const withdrawToken = (dispatch, exchange, web3, token, amount, account) 
   exchange.methods.withdrawToken(token.options.address, web3.utils.toWei(amount, 'ether')).send({ from: account })
   .on('transactionHash', (hash) => {
     dispatch(balancesLoading())
+  })
+  .on('error',(error) => {
+    console.error(error)
+    window.alert(`There was an error!`)
+  })
+}
+
+export const makeBuyOrder = (dispatch, exchange, token, web3, order, account) => {
+  const tokenGet = token.options.address
+  const amountGet = web3.utils.toWei(order.amount, 'ether')
+  const tokenGive = ETHER_ADDRESS
+  const amountGive = web3.utils.toWei((order.amount * order.price).toString(), 'ether')
+
+  exchange.methods.makeOrder(tokenGet, amountGet, tokenGive, amountGive).send({ from: account })
+  .on('transactionHash', (hash) => {
+    dispatch(buyOrderMaking())
+  })
+  .on('error',(error) => {
+    console.error(error)
+    window.alert(`There was an error!`)
+  })
+}
+
+export const makeSellOrder = (dispatch, exchange, token, web3, order, account) => {
+  const tokenGet = ETHER_ADDRESS
+  const amountGet = web3.utils.toWei((order.amount * order.price).toString(), 'ether')
+  const tokenGive = token.options.address
+  const amountGive = web3.utils.toWei(order.amount, 'ether')
+
+  exchange.methods.makeOrder(tokenGet, amountGet, tokenGive, amountGive).send({ from: account })
+  .on('transactionHash', (hash) => {
+    dispatch(sellOrderMaking())
   })
   .on('error',(error) => {
     console.error(error)
